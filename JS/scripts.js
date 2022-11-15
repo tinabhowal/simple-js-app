615b7d2defd2825a55a1ca09b00547c3d4be18c3
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2022-11-15 18:13:53 +0100

    task 1.2

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,6 @@
-alert("Hello World!");
+// alert("Hello World!");
 
-let favoriteFood = "Paneer";
-document.write(favoriteFood);
+// let favoriteFood = "Paneer";
+// document.write(favoriteFood);
 
+let pokemonList=[{name:"Caterpie", height:7, type: ["grass","water"] }, {name:"Weedle", height:5, type: ["sweet","salty"] }, {name:"Pidgeotto", height:6, type: ["grass","air"] } ]
