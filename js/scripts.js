a6f0f496f30d50352759349861a2b43f0a1148dd
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2022-11-29 19:39:33 +0100

    task 1.7

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ let pokemonRepository = (function () {
  
 let pokemonList =     
 [
-    {name:"Caterpie", height:7, type: ["grass","water"] }, 
-    {name:"Weedle", height:5, type: ["sweet","salty"] }, 
-    {name:"Pidgeotto", height:6, type: ["grass","air"] }
+    // {name:"Caterpie", height:7, type: ["grass","water"] }, 
+    // {name:"Weedle", height:5, type: ["sweet","salty"] }, 
+    // {name:"Pidgeotto", height:6, type: ["grass","air"] }
 
 ];
+
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 function add(pokemon) {
     pokemonList.push(pokemon);
@@ -29,16 +31,52 @@ function addListItem(pokemon){
       });
 }
 
-function showDetails(pokemon){
-    console.log(pokemon.name);
+
+
+function loadList(){
+    return fetch(apiUrl).then(function (response){
+        return response.json();
+    }).then(function (json) {
+        json.results.forEach(function (item){
+            let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            add(pokemon);
+        });
+    }).catch(function(e){
+        console.error(e);
+    })
 }
 
+
+function loadDetails (item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response){
+        return response.json();
+    }).then(function(details){
+       item.imageUrl = details.sprites.front_default;
+       item.height = details.height;
+       item.types = details.types;
+    }).catch(function (e) {
+        console.error(e);
+    });
+}
+
+
+function showDetails(pokemon){
+    loadDetails(pokemon).then(function(){
+        console.log(pokemon);
+    });
+}
 
 
 return{
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
     showDetails:showDetails
 };
 
@@ -46,11 +84,17 @@ return{
 
 })();
 
-pokemonRepository.add({name:"Pikachu", height:7, type:["grass" , "water"]})
+// pokemonRepository.add({name:"Pikachu", height:7, type:["grass" , "water"]})
 
-pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function ()
+{
+    pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
+
 });
+
+
 
 
 
